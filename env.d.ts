977f4338078b11df94a8fc3c226f8682e172cac5
deatiger <deatiger1492@gmail.com>
2024-12-05 09:27:56 +0900

    Fix

--- env.d.ts
+++
@@ -1,6 +0,0 @@
-declare namespace NodeJS {
-  interface ProcessEnv {
-    MICROCMS_SERVICE_DOMAIN: string;
-    MICROCMS_API_KEY: string;
-  }
-}
--- src/services/microcms/client.ts
+++ src/services/microcms/client.ts
@@ -1,6 +1,5 @@
 import { createClient } from 'microcms-js-sdk';
 
-// Only create the client if configuration is valid
 export const client = createClient({
   serviceDomain: process.env.MICROCMS_SERVICE_DOMAIN ?? '',
   apiKey: process.env.MICROCMS_API_KEY ?? '',
